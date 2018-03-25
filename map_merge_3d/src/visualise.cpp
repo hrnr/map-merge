be7846650c44e3fe1c84cb844f93d572edf841bc
Jiri Horner <laeqten@gmail.com>
2018-03-25 16:44:46 +0200

    workaround bug in visualiser

--- map_merge_3d/src/visualise.cpp
+++ map_merge_3d/src/visualise.cpp
@@ -2,50 +2,54 @@
 
 #include <pcl/visualization/pcl_visualizer.h>
 
+// use just one instance of vis to overcome bug PCL #172
+static pcl::visualization::PCLVisualizer vis("cloud view");
+
+static void show() {
+  vis.spin();
+  vis.removeCorrespondences();
+  vis.removeAllPointClouds();
+  vis.removeAllShapes();
+  vis.removeAllCoordinateSystems();
+}
+
 void visualisePointClouds(PointCloudPtr cloud1, PointCloudPtr cloud2)
 {
   std::cout << "displaying pointclouds of sizes: " << cloud1->size() << ", "
             << cloud2->size() << std::endl;
-  pcl::visualization::PCLVisualizer vis("pointclouds match");
+
   ColorHandlerT green(cloud1, 0.0, 255.0, 0.0);
   vis.addPointCloud(cloud1, green, "cloud1");
   ColorHandlerT blue(cloud2, 0.0, 0.0, 255.0);
   vis.addPointCloud(cloud2, blue, "cloud2");
-  vis.spin();
-  vis.close();
+  show();
 }
 
 void visualisePointCloud(PointCloudPtr cloud)
 {
   std::cout << "displaying pointcloud of size: " << cloud->size() << std::endl;
-  pcl::visualization::PCLVisualizer vis("pointcloud");
 
   ColorHandlerT green(cloud, 0.0, 255.0, 0.0);
   vis.addPointCloud(cloud, green, "cloud");
-  vis.spin();
-  vis.close();
-  vis.spinOnce();
+
+  show();
 }
 
 void visualiseNormals(PointCloudPtr cloud, SurfaceNormalsPtr normals)
 {
   std::cout << "displaying pointcloud of size: " << cloud->size() << std::endl;
-  pcl::visualization::PCLVisualizer vis("pointcloud and normals");
 
   ColorHandlerT green(cloud, 0.0, 255.0, 0.0);
   vis.addPointCloud(cloud, green, "cloud");
   vis.addPointCloudNormals<PointT, NormalT>(cloud, normals, 5, 0.1f, "normals");
 
-  vis.spin();
-  vis.close();
-  vis.spinOnce();
+  show();
 }
 
 void visualiseKeypoints(PointCloudPtr cloud, PointCloudPtr keypoints)
 {
   std::cout << "pointcloud size: " << cloud->size() << std::endl
             << "keypoints: " << keypoints->size() << std::endl;
-  pcl::visualization::PCLVisualizer vis("pointcloud keypoints");
 
   ColorHandlerT green(cloud, 0.0, 255.0, 0.0);
   vis.addPointCloud(cloud, green, "cloud");
@@ -55,9 +59,7 @@ void visualiseKeypoints(PointCloudPtr cloud, PointCloudPtr keypoints)
   vis.setPointCloudRenderingProperties(
       pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 5, "keypoints");
 
-  vis.spin();
-  vis.close();
-  vis.spinOnce();
+  show();
 }
 
 void visualiseCorrespondences(PointCloudPtr cloud1, PointCloudPtr keypoints1,
@@ -69,7 +71,6 @@ void visualiseCorrespondences(PointCloudPtr cloud1, PointCloudPtr keypoints1,
   std::cout << "keypoints: " << keypoints1->size() << ", " << keypoints2->size()
             << std::endl;
   std::cout << "correspondences: " << correspondences->size() << std::endl;
-  pcl::visualization::PCLVisualizer vis("pointcloud keypoints");
 
   ColorHandlerT green(cloud1, 0.0, 255.0, 0.0);
   vis.addPointCloud(cloud1, green, "cloud1");
@@ -78,7 +79,5 @@ void visualiseCorrespondences(PointCloudPtr cloud1, PointCloudPtr keypoints1,
 
   vis.addCorrespondences<PointT>(keypoints1, keypoints2, *correspondences);
 
-  vis.spin();
-  vis.close();
-  vis.spinOnce();
+  show();
 }
