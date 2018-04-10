448fa251a67593f44767c7e7abab5d217c625664
Jiri Horner <laeqten@gmail.com>
2018-04-10 18:12:28 +0200

    use MapMergingParams in registration_visualisation

--- map_merge_3d/src/registration_visualisation.cpp
+++ map_merge_3d/src/registration_visualisation.cpp
@@ -1,6 +1,7 @@
 #include <map_merge_3d/typedefs.h>
 
 #include <map_merge_3d/features.h>
+#include <map_merge_3d/map_merging.h>
 #include <map_merge_3d/matching.h>
 #include <map_merge_3d/visualise.h>
 
@@ -9,7 +10,7 @@
 #include <pcl/console/parse.h>
 #include <pcl/io/pcd_io.h>
 
-void printPointCloud2Summary(const pcl::PCLPointCloud2 &v)
+static inline void printPointCloud2Summary(const pcl::PCLPointCloud2 &v)
 {
   std::cout << "header: " << std::endl;
   std::cout << v.header;
@@ -23,117 +24,99 @@ void printPointCloud2Summary(const pcl::PCLPointCloud2 &v)
     std::cout << std::endl;
     std::cout << "    " << v.fields[i] << std::endl;
   }
-  std::cout << "is_bigendian: ";
-  std::cout << "  " << v.is_bigendian << std::endl;
-  std::cout << "point_step: ";
-  std::cout << "  " << v.point_step << std::endl;
-  std::cout << "row_step: ";
-  std::cout << "  " << v.row_step << std::endl;
-  std::cout << "is_dense: ";
-  std::cout << "  " << v.is_dense << std::endl;
 }
 
 int main(int argc, char **argv)
 {
-  double resolution = 0.1;
-  int min_neighbours = 50;
-  double keypoint_threshold = 5.0;
-  double normal_radius = resolution * 6.;
-  double descriptor_radius = resolution * 8.;
-  double inlier_threshold = resolution * 5.;
-  const double max_correspondence_distance = inlier_threshold * 2.;
-  const int nr_iterations = 1000;
-  const int max_iterations = 200;
-  std::string descriptor_name = "PFH";
-  std::string keypoint_name = "SIFT";
-  int matching_k = 5;
+  std::vector<int> pcd_file_indices =
+      pcl::console::parse_file_extension_argument(argc, argv, ".pcd");
+  if (pcd_file_indices.size() != 2) {
+    pcl::console::print_error("Need exactly 2 input files!\n");
+    return -1;
+  }
 
-  pcl::console::parse_argument(argc, argv, "--normal_radius", normal_radius);
-  pcl::console::parse_argument(argc, argv, "--descriptor_radius",
-                               descriptor_radius);
-  pcl::console::parse_argument(argc, argv, "--resolution", resolution);
-  pcl::console::parse_argument(argc, argv, "--min_neighbours", min_neighbours);
-  pcl::console::parse_argument(argc, argv, "--descriptor", descriptor_name);
-  pcl::console::parse_argument(argc, argv, "--keypoint_type", keypoint_name);
-  pcl::console::parse_argument(argc, argv, "--matching_k", matching_k);
-  pcl::console::parse_argument(argc, argv, "--inlier_threshold",
-                               inlier_threshold);
-  pcl::console::parse_argument(argc, argv, "--keypoint_threshold",
-                               keypoint_threshold);
+  const MapMergingParams params = MapMergingParams::fromCommandLine(argc, argv);
 
   PointCloudPtr cloud1(new PointCloud);
   PointCloudPtr cloud2(new PointCloud);
 
   // Load object and scene
-  if (pcl::io::loadPCDFile<PointT>(argv[1], *cloud1) < 0 ||
-      pcl::io::loadPCDFile<PointT>(argv[2], *cloud2) < 0) {
-    pcl::console::print_error("Error loading object/scene file!\n");
-    return (1);
+  if (pcl::io::loadPCDFile<PointT>(argv[pcd_file_indices[0]], *cloud1) < 0 ||
+      pcl::io::loadPCDFile<PointT>(argv[pcd_file_indices[1]], *cloud2) < 0) {
+    pcl::console::print_error("Error loading input file!\n");
+    return -1;
   }
-  pcl::console::print_highlight("Pointclouds loaded.\n");
 
-  cloud1 = downSample(cloud1, resolution);
-  cloud2 = downSample(cloud2, resolution);
+  cloud1 = downSample(cloud1, params.resolution);
+  cloud2 = downSample(cloud2, params.resolution);
 
   visualisePointCloud(cloud1);
 
-  cloud1 = removeOutliers(cloud1, descriptor_radius, min_neighbours);
-  cloud2 = removeOutliers(cloud2, descriptor_radius, min_neighbours);
+  cloud1 = removeOutliers(cloud1, params.descriptor_radius,
+                          params.outliers_min_neighbours);
+  cloud2 = removeOutliers(cloud2, params.descriptor_radius,
+                          params.outliers_min_neighbours);
 
   visualisePointCloud(cloud1);
 
   /* detect normals */
+  pcl::console::print_highlight("Computing normals.\n");
   SurfaceNormalsPtr normals1, normals2;
   {
     pcl::ScopeTime t("normals computation");
-    normals1 = computeSurfaceNormals(cloud1, normal_radius);
-    normals2 = computeSurfaceNormals(cloud2, normal_radius);
+    normals1 = computeSurfaceNormals(cloud1, params.normal_radius);
+    normals2 = computeSurfaceNormals(cloud2, params.normal_radius);
   }
 
   visualiseNormals(cloud1, normals1);
 
   /* detect keypoints */
+  pcl::console::print_highlight("Detecting keypoints.\n");
   PointCloudPtr keypoints1, keypoints2;
-  Keypoint keypoint_type = keypointType(keypoint_name);
   {
     pcl::ScopeTime t("keypoints detection");
-    keypoints1 = detectKeypoints(cloud1, normals1, keypoint_type,
-                                 keypoint_threshold, normal_radius, resolution);
-    keypoints2 = detectKeypoints(cloud2, normals2, keypoint_type,
-                                 keypoint_threshold, normal_radius, resolution);
+    keypoints1 = detectKeypoints(cloud1, normals1, params.keypoint_type,
+                                 params.keypoint_threshold,
+                                 params.normal_radius, params.resolution);
+    keypoints2 = detectKeypoints(cloud2, normals2, params.keypoint_type,
+                                 params.keypoint_threshold,
+                                 params.normal_radius, params.resolution);
   }
 
   visualiseKeypoints(cloud1, keypoints1);
 
   /* compute descriptors */
+  pcl::console::print_highlight("Computing descriptors.\n");
   LocalDescriptorsPtr descriptors1, descriptors2;
-  Descriptor descriptor_type = descriptorType(descriptor_name);
   {
     pcl::ScopeTime t("descriptors computation");
     descriptors1 = computeLocalDescriptors(cloud1, normals1, keypoints1,
-                                           descriptor_type, descriptor_radius);
+                                           params.descriptor_type,
+                                           params.descriptor_radius);
     descriptors2 = computeLocalDescriptors(cloud2, normals2, keypoints2,
-                                           descriptor_type, descriptor_radius);
+                                           params.descriptor_type,
+                                           params.descriptor_radius);
   }
 
   std::cout << "extracted descriptors:" << std::endl;
   printPointCloud2Summary(*descriptors1);
 
   /* compute correspondences */
+  pcl::console::print_highlight("Transform estimation using MATCHING.\n");
   CorrespondencesPtr inliers;
   Eigen::Matrix4f transform;
-
   {
     pcl::ScopeTime t("finding correspondences");
     CorrespondencesPtr correspondences = findFeatureCorrespondences(
-        descriptors1, descriptors2, size_t(matching_k));
-    transform = estimateTransformFromCorrespondences(
-        keypoints1, keypoints2, correspondences, inliers, inlier_threshold);
+        descriptors1, descriptors2, params.matching_k);
+    transform = estimateTransformFromCorrespondences(keypoints1, keypoints2,
+                                                     correspondences, inliers,
+                                                     params.inlier_threshold);
   }
 
-  std::cout << "matching est score: "
+  std::cout << "MATCHING est score: "
             << transformScore(cloud1, cloud2, transform,
-                              max_correspondence_distance)
+                              params.max_correspondence_distance)
             << std::endl;
 
   visualiseCorrespondences(cloud1, keypoints1, cloud2, keypoints2, inliers);
@@ -143,33 +126,37 @@ int main(int argc, char **argv)
   pcl::transformPointCloud(*cloud1, *cloud1_aligned, transform);
   visualisePointClouds(cloud1_aligned, cloud2);
 
+  pcl::console::print_highlight("Transform estimation using SAC_IA.\n");
+  Eigen::Matrix4f transform_ia;
   {
     pcl::ScopeTime t("initial alignment");
-    transform = estimateTransformFromDescriptorsSets(
-        keypoints1, descriptors1, keypoints2, descriptors2, inlier_threshold,
-        max_correspondence_distance, nr_iterations);
+    transform_ia = estimateTransformFromDescriptorsSets(
+        keypoints1, descriptors1, keypoints2, descriptors2,
+        params.inlier_threshold, params.max_correspondence_distance,
+        params.max_iterations);
   }
 
   std::cout << "SAC_IA est score: "
-            << transformScore(cloud1, cloud2, transform,
-                              max_correspondence_distance)
+            << transformScore(cloud1, cloud2, transform_ia,
+                              params.max_correspondence_distance)
             << std::endl;
 
   // Transform the source point to align them with the target points
-  pcl::transformPointCloud(*cloud1, *cloud1_aligned, transform);
+  pcl::transformPointCloud(*cloud1, *cloud1_aligned, transform_ia);
   visualisePointClouds(cloud1_aligned, cloud2);
 
-  PointCloudPtr aligned(new PointCloud);
+  pcl::console::print_highlight("Refining transform with ICP.\n");
   {
     pcl::ScopeTime t("ICP alignment");
-    transform = estimateTransformICP(cloud1, cloud2, transform,
-                                     max_correspondence_distance,
-                                     inlier_threshold, max_iterations, 1e-3);
+    transform = estimateTransformICP(
+        cloud1, cloud2, transform, params.max_correspondence_distance,
+        params.inlier_threshold, params.max_iterations,
+        params.transform_epsilon);
   }
 
   std::cout << "ICP est score: "
             << transformScore(cloud1, cloud2, transform,
-                              max_correspondence_distance)
+                              params.max_correspondence_distance)
             << std::endl;
 
   pcl::transformPointCloud(*cloud1, *cloud1_aligned, transform);
